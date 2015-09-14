a6380c79ccf1412625ead688df1019226f197503
ThermalSpan <russell.bentley@Uconn.edu>
2015-09-13 22:44:41 -0400

    Oops, forgot the stdafx.h for the experiments directory

---
+++ Experiments/stdafx.h
@@ -0,0 +1,9 @@
+// This file was created by Russell Bentley
+// Copyright 2015
+// Distributed under the MIT License. 
+
+#pragma once
+
+#include <iostream>
+#include <stdlib.h>
+#include <assert.h>
